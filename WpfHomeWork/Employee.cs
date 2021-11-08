5b04ba4e5b5f2b358e6bac6915c14d35781ca4c7
Alex Z <can4you@yandex.ru>
2021-11-08 21:33:24 +0300

    Добавьте файлы проекта.

---
+++ WpfHomeWork/Employee.cs
@@ -0,0 +1,43 @@
+﻿using WpfHomeWork.Implementations;
+
+namespace WpfHomeWork
+{
+    internal class Employee : ViewModel
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string OutputData => Name + " - " + Salary;
+        public Employee Left { get; set; }
+        public Employee Right { get; set; }
+
+        public static void Add(Employee current, Employee toAdd)
+        {
+            if (toAdd.Salary < current.Salary)
+            {
+                if (current.Left != null)
+                {
+                    Add(current.Left, toAdd);
+                }
+
+                else
+                {
+                    current.Left = toAdd;
+                }
+            }
+
+            else
+            {
+                if (current.Right != null)
+                {
+                    Add(current.Right, toAdd);
+                }
+
+                else
+                {
+                    current.Right = toAdd;
+                }
+            }
+        }
+
+    }
+}
---
+++ WpfHomeWork/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using WpfHomeWork.Implementations;
+
+namespace WpfHomeWork
+{
+    internal class MainViewModel : ViewModel
+    {
+        public MainViewModel()
+        {
+            Employees = new ObservableCollection<Employee>();
+        }
+
+        public ICommand AddEmployeeCommand { get { return new Command(AddEmployee); } }
+        public ICommand CreateTreeCommand { get { return new Command(CreateTree); } }
+        public ObservableCollection<Employee> Employees { get; private set; }
+
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public Employee TreeEmployee = null;
+
+        private void AddEmployee()
+        {
+            var employee = new Employee() { Name = this.Name, Salary = this.Salary };
+            Employees.Add(employee);
+
+            Name = null;
+            Salary = 0;
+            RaisePropertyChanged(() => Name);
+            RaisePropertyChanged(() => Salary);
+        }
+
+        private void CreateTree()
+        {
+            Employee employee = null;
+
+            for (int i = 0; i < Employees.Count; i++)
+            {
+                if (employee == null)
+                {
+                    employee = new Employee { Name = Employees[i].Name, Salary = Employees[i].Salary };
+                }
+
+                else
+                {
+                    Employee.Add(employee, new Employee { Name = Employees[i].Name, Salary = Employees[i].Salary });
+                }
+            }
+
+            TreeEmployee = employee;
+        }
+
+    }
+}
